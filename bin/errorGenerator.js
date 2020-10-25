257b6984c47784eb17b04156436624f25b46ea12
iamrestrepo <monica.restrepo@mlssoccer.com>
2020-10-25 17:30:12 -0400

    fix(error-generator: adds random function to pick errors and fixes readme grammar

--- bin/errorGenerator.js
+++ bin/errorGenerator.js
@@ -1,7 +1,17 @@
 
-//Temp Error
+//Call randomly for erroneous functions
+const getRandomError = () => {
+    let randomNumber = Math.random() * 3
+    if(randomNumber === 1){
+        mergeSort([])
+    }else if(randomNumber === 2){
+        merge([23, 2, 56, 88, 6, 134, 3])
+    }else {
+        swap(4, 1, [23, 2, 56, 88, 6, 134, 3])
+    }
+}
 
-function mergeSort(arr) {
+const mergeSort = (arr) => {
     if (arr.length <= 1) {
         return arr;
     }
@@ -11,7 +21,7 @@ function mergeSort(arr) {
     return merge(mergeSort(leftPart), mergeSort(rightPart));
 }
 
-function merge(left, right) 
+const merge = (left, right) => {
     let mergedArr = [];
     while (left.length > 0 && right.length > 0) {
         if (left[0] < right[0]) {
@@ -25,16 +35,16 @@ function merge(left, right)
     return mergedArr.concat(left).concat(right);
 }
 
-console.log(mergeSort([23, 2, 56, 88, 6, 134, 3]));
 
 
-function swap(index1, index2, array) {
+const swap = (index1, index2, array) => {
     let temp = array[index1];
     array[index1] = array[index2];
     array[index2] = temp;
     return array;
 }
-function bubbleSort(array) {
+
+const bubbleSort = (array) =>  {
     let arrLimit = array.length - 1;
     while (arrLimit > 0) {
         for (let i = 0; i < arrLimit; i += 1) {
@@ -42,7 +52,7 @@ function bubbleSort(array) {
                 array = swap(i, i + 1, array);
             }
         }
-        arrLimit = arrLimit - 1;
+        arrLimit = arrLimit - ;
     }
     return array;
 }
